3c26501f929f75386ad444eaf06a6842d63ebf4b
TamoorKhokhar <TamoorIsalam076@gmail.com>
2022-02-02 17:01:37 +0500

    updatestateusinghooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// useState is hook which allow us to maintain state or update state
 
+import { useState } from "react";
 function App() {
+  //How to change data using function and click event
+  // let data = "Tamoor Khokhar";
+  // function green(props) {
+  //   data = "TAMOOR";
+  //   alert(data);
+  // }
+  // update state using hooks
+  const [data, setState] = useState("Anil");
+  function updateData() {
+    setState("Tamoor");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <p>How to change data using function and click event</p>
+    //   <h1>{data}</h1>
+    //   <button onClick={() => green()}>CLICK ME</button>
+    // </div>
+
+    //update state
+    <div>
+      <h1>{data}</h1>
+      <button onClick={updateData}>UPDATE ME</button>
     </div>
   );
 }
-
 export default App;
